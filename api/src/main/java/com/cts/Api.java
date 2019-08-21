03b2346a766d7dbbc1956598d80e7032bc5dbf7e
antony <mark.antonyi@cognizant.com>
2019-08-21 11:43:39 +0530

    test

--- api/src/main/java/com/cts/Api.java
+++ api/src/main/java/com/cts/Api.java
@@ -6,8 +6,7 @@ package com.cts;
 public class Api {
 
     public int apiSum(int a, int b) {
-    	System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String) 
-        return a + b;  
+    	         return a + b;  
  }
  
  public int apiSubtraction(int a, int b) {
