87c9c47134f188964b8612bb90ca6965c78cb2e8
antony <mark.antonyi@cognizant.com>
2019-08-23 14:13:10 +0530

    test

--- dao/src/main/java/com/cts/Dao.java
+++ dao/src/main/java/com/cts/Dao.java
@@ -10,7 +10,6 @@ public class Dao {
  }
  
  public int daoSubtraction(int a, int b) {
-	 System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
      return a - b;
  }
  
