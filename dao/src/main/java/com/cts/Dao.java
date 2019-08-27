54cb9831a0245557ca832d09b12b1104fb916212
antony <mark.antonyi@cognizant.com>
2019-08-27 10:32:00 +0530

    test

--- dao/src/main/java/com/cts/Dao.java
+++ dao/src/main/java/com/cts/Dao.java
@@ -6,6 +6,7 @@ package com.cts;
 public class Dao {
 
     public int daoSum(int a, int b) {
+    	 System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
         return a + b;  
  }
  
