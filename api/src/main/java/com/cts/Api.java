95f2c36f0fd248aca7a565421b19c9ea3cc44f19
NithyaRajamanickam <40426789+NithyaRajamanickam@users.noreply.github.com>
2019-08-29 14:13:54 +0530

    test branch commit

--- api/src/main/java/com/cts/Api.java
+++ api/src/main/java/com/cts/Api.java
@@ -10,6 +10,7 @@ public class Api {
  }
  
  public int apiSubtraction(int a, int b) {
+	 System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
      return a - b;
  }
  
