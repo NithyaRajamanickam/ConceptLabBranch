9a9f8970320b584a1493b374c3fa369ec025bff4
nithiya <nithiya.rajamanickam@cognizant.com>
2019-09-11 12:05:07 +0530

    Test defect  124

--- api/src/main/java/com/cts/Api.java
+++ api/src/main/java/com/cts/Api.java
@@ -6,9 +6,24 @@ package com.cts;
 public class Api {	
 	
 	    public int apiSum(int a, int b) {
+	    	
+	    	int param = 1;
+			if (param == 1)
+			 System.out.println("foo");
+			else if (param == 2)
+			 System.out.println("foo");
+			else if (param == 1) 
+			{// Noncompliant
+			  System.out.println("foo");
+			}
+			
+	 
     	         return a + b;  
  }
     public int apiSum2(int a, int b) {
+    	
+    	 
+    		System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
 
     	int param = 1;
 		
