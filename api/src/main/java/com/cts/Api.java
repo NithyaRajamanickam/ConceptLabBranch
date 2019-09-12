f2be59fdf8df8ff9eec1d82d6a9eabe3533722e5
nithiya <nithiya.rajamanickam@cognizant.com>
2019-09-12 09:59:11 +0530

    test

--- api/src/main/java/com/cts/Api.java
+++ api/src/main/java/com/cts/Api.java
@@ -1,44 +1,35 @@
 package com.cts;
 
-
-
-
 public class Api {	
 	
-	    public int apiSum(int a, int b) {	    	
-	    	
-    	         return a + b;  
- }
-    public int apiSum2(int a, int b) {
-    	
-    	 
-    	
-
-    	int param = 1;
-		
-        return a + b;  
-}
- 
- public int apiSubtraction(int a, int b) {
-     return a - b;
- }
- 
- public int apiMultiplication(int a, int b) {
-        return a * b;
- }
-
- public int apiDivison(int a, int b)  {
-        return a / b;
- }
- 
- public boolean equalIntegers2(int a, int b) {
-        boolean result = false;
-        if (a == b) {
-               result = true;
-        }
-
-        return result;
- }
-
+	 public int apiSum(int a, int b) {	    	
+	    return a + b;  
+	 }
+	 
+	 public int apiSum2(int a, int b) {
+		int param = 1;
+	    return a + b;  
+	}
+	 
+	 public int apiSubtraction(int a, int b) {
+		 System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
+	     return a - b;
+	 }
+	 
+	 public int apiMultiplication(int a, int b) {
+	        return a * b;
+	 }
 	
+	 public int apiDivison(int a, int b)  {
+	        return a / b;
+	 }
+	 
+	 public boolean equalIntegers2(int a, int b) {
+	        boolean result = false;
+	        if (a == b) {
+	               result = true;
+	        }
+	
+	        return result;
+	 }
 }
